9a6c06fe4bdb42f2a855384d84d156c4b32cd1ac
randyagaleano <randyagaleano@gmail.com>
2017-03-01 16:37:52 -0500

    Fixed traffic lights

---
+++ dom_intro_lesson/main.js
@@ -0,0 +1,17 @@
+// document.ready(){} Also works
+window.onload = function () {
+  var response = prompt("Hi! What's your favorite food?");
+  alert(`I wish I liked ${response} too. Sometimes, it is sad to be a computer. :( `);  
+}
+
+function(a, b) {
+	console.log(a+b);
+}
+
+function sum(a,b) {
+	console.log(a+b);
+}
+
+var newFunction = function() {
+	console.log(a+b);
+}
--- unit_01/w02d02/student_labs/independent_click_events/traffic_light/app.js
+++ unit_01/w02d02/student_labs/independent_click_events/traffic_light/app.js
@@ -1,12 +1,16 @@
 window.onload = function() {
   var stopButton = document.querySelector('#stopButton');
   var slowButton = document.querySelector('#slowButton');
+  var goButton = document.querySelector('#goButton');
 
   var stopLight = document.querySelector('#stopLight');
   var slowLight = document.querySelector('#slowLight');
+  var goLight = document.querySelector('#goLight')
 
   stopButton.addEventListener('click', trafficLight.illuminateRed);
-  slowButton.addEventListener('click', trafficLight.illuminateYellow);
+  slowButton.addEventListener('click', 
+    trafficLight.illuminateYellow);
+  goButton.addEventListener('click', trafficLight.illuminateGreen);
 }
 
 
@@ -20,11 +24,18 @@ var trafficLight = {
   illuminateYellow: function(event) {
     trafficLight.clearLights();
 
-    slowLight.style.background = 'blue';
+    slowLight.style.background = 'Yellow';
   },
+  illuminateGreen: function(event) {
+    trafficLight.clearLights();
+
+    goLight.style.background = 'green'
+  }, 
 
   clearLights: function() {
     stopLight.style.background = 'black';
+    slowLight.style.background = 'black';
+    goLight.style.background = 'black';
 
   }
 }
