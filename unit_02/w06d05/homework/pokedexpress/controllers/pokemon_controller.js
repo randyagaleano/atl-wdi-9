fe7040fbad26543c2039ca59c03b161072f2732c
randyagaleano <randyagaleano@gmail.com>
2017-03-19 15:05:42 -0400

    pre styling

--- unit_02/w06d05/homework/pokedexpress/controllers/pokemon_controller.js
+++ unit_02/w06d05/homework/pokedexpress/controllers/pokemon_controller.js
@@ -10,16 +10,33 @@ router.get('/', function(req, res) {
   })
 });
 
-// router.get('/:id', function(req, res) {
-//   var index = req.params.id - 1;
-//   var currentPokemon = pokeList[index];
 
-//   console.log(currentPokemon.misc);
-//   res.render('show.hbs', {
-//     pokemon: currentPokemon
-//   })
-//   console.log(index);
-// });
+router.get('/new', function (req, res) {
+	res.render('new');
+});
+
+router.post('/', function (req, res) {
+	pokeData.push({
+		name: req.body.name,
+		id: req.body.id,
+		type: req.body.type,
+	});
+	res.redirect('/pokemon')
+});
+
+
+router.get('/:id', function(req, res) {
+	var pokemon = pokeData[req.params.id];
+
+	res.render('show', {
+		name: pokemon.name,
+		id: pokemon.id,
+		type: pokemon.type,
+		img: pokemon.img,
+		stats: pokemon.stats,
+		// add link to moves where user can see complete moveset
+	});
+});
 
 
 
@@ -31,67 +48,3 @@ router.get('/', function(req, res) {
 module.exports = router;
 
 
-
-//***************************
-// REQUIREMENTS
-//***************************
-// Set up your express dependency here:
-
-// Set express Router to a variable called router:
-
-// Let's export this router file at the bottom of the page:
-// (Scroll to bottom to Exports)
-
-// Require the poke_array.js file here from models.
-// Save it to a variable called data:
-
-
-
-
-//***************************
-// READ
-//***************************
-// Note: All the routes below can be accessed at `localhost:3000/pokemon` + `resource`
-// Make a GET route '/' that will render an index page of all pokemon images
-
-
-
-
-
-// Make a GET route '/index/:index' that will render the Pokemon's show page at that :index
-//
-// Example: a user goes to 'localhost:3000/pokemon/index/0' in the browser and data for Bulbasaur (the pokemon at index 0) will be displayed.
-
-
-
-
-// Make a GET route '/new' that will simply render a form to create a new Pokemon
-
-
-
-
-
-
-
-//***************************
-// CREATE
-//***************************
-//make a POST route '/' to create a New Pokemon
-
-
-//***************************
-// UPDATE
-//***************************
-
-//***************************
-// DELETE
-//***************************
-//make a DELETE route '/:index' that will delete the Pokemon at this index.
-
-
-
-//***************************
-// EXPORTS
-//***************************
-// use module.exports to export router:
-// this makes the scripts on this page accessible by other files that "require" this file. Without exporting, the code in here just sits in here, alone.
