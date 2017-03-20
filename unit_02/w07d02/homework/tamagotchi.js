c7ce9b904eb9dafbe3f271684356f17aa2c9af4d
randyagaleano <randyagaleano@gmail.com>
2017-03-20 18:20:14 -0400

    part 1: created tamagotchi constructors

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -1,5 +1,28 @@
 console.log('tamagotchi file is loaded');
+var Tamagotchi = function() {
 
+  this.foodInTummy = 10;
+  this.restedness = 10;
+  this.health = 10;
+
+  this.cry = function() {
+      this.foodInTummy--;
+      console.log("The Tamagotchi is crying!!! WAAAH!!!!!!");
+      console.log('current food in tummy: ' + this.foodInTummy);
+  };
+};
+
+//create a new Tamagotchi with `new`
+var Tamagotchi1 = new Tamagotchi();
+var Tamagotchi2 = new Tamagotchi();
+
+//console log the new Objects to check them out
+console.log(Tamagotchi1);
+console.log(Tamagotchi2);
+
+//Invoke methods on the constructed objects:
+Tamagotchi1.cry();
+Tamagotchi2.cry();
 //your constructor function here
 
 
