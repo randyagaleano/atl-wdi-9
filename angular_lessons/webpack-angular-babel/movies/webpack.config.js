5f4fa167b7bebcd10801f8d286fbf41f3bfad31f
randyagaleano <randyagaleano@gmail.com>
2017-04-05 11:16:23 -0400

    first movies app

--- angular_lessons/webpack-angular-babel/movies/webpack.config.js
+++ angular_lessons/webpack-angular-babel/movies/webpack.config.js
@@ -4,10 +4,19 @@ var glob = require('glob');
 module.exports = {
     context: __dirname,
     entry: {
-        app: glob.sync(__dirname + '/public/js/**/*.js')
+        app: glob.sync(__dirname + '/client/**/*.js')
     },
     output: {
         path: __dirname + '/public/js',
         filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
     }
 };
